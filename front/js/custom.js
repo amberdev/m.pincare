492477805826e76f727a532a407951155d9c4e61
Dipanshu <gupta.dipanshu@rediffmail.com>
2017-02-13 18:32:10 +0530

    changes

--- front/js/custom.js
+++ front/js/custom.js
@@ -72,19 +72,19 @@ var base_url="http://m.pincare.in/";
 	function checkin(place_id,outlet_id)
 	{
 		// alert("checkin");
-	 //    FB.api('/me/checkins', 'post', 
-	 //    { message: 'Testing checkins',
-	 //       place: 149296708507748,
-	 //       coordinates: {
-	 //           'latitude': 28.637010722237,
-	 //           'longitude': 77.286376153576
-	 //       }
-	 //    },
-	 //        function (response) {
+	    // FB.api('/me/checkins', 'post', 
+	    // { message: 'Testing checkins',
+	    //    place: 149296708507748,
+	    //    coordinates: {
+	    //        'latitude': 28.637010722237,
+	    //        'longitude': 77.286376153576
+	    //    }
+	    // },
+	    //     function (response) {
 	            
-	 //            console.log(response);
-	 //        }
-	 //    );
+	    //         console.log(response);
+	    //     }
+	    // );
 		FB.getLoginStatus(function (response) 
     {
     if (response.status === 'connected') 
@@ -92,10 +92,27 @@ var base_url="http://m.pincare.in/";
 	FB.api('/me/accounts', function(response) {
 	 
 	var access_token1 =   FB.getAuthResponse()['accessToken'];
-	console.log(access_token1);
-	console.log(response);
+	console.log(response.data[0].access_token);
 
-		
+	FB.api('/me/feed', 'post', 
+              { 
+                  // for the newer versions of the Facebook API you need to add the access token
+                  access_token: response.data[0].access_token,
+                  message     : "Hii Testing with schedule time",
+                  link        : 'https://developers.facebook.com/docs/reference/dialogs/',
+                  picture     : 'http://fbrell.com/f8.jpg',
+                  name        : 'Featured of the Day',
+		         
+                  description : 'Your description'
+          }, 
+          function(response) {
+
+              if (!response || response.error) {
+                  alert(JSON.stringify(response.error));
+              } else {
+                  alert('Post ID: ' + response.id);
+              }
+          });
 	 
 	 
 	});    
