cd46458b84d33e8b9bb5bf57abe0db83e08f4290
Dipanshu <gupta.dipanshu@rediffmail.com>
2017-02-16 20:02:56 +0530

    privacy policy

--- application/views/include/menu.php
+++ application/views/include/menu.php
@@ -6,5 +6,6 @@
         <li><a href="<?php echo base_url();?>story/score" class="score-link">SCORE</a></li>
         <li><a href="<?php echo base_url();?>story/trandingpins" class="trending-link">TRENDING PINS</a></li>
         <li><a href="<?php echo base_url();?>">HOME</a></li>
+        <li><a href="<?php echo base_url();?>front/privacy-policy.pdf">Privacy Policy</a></li>
     </ul>
 </div> 
